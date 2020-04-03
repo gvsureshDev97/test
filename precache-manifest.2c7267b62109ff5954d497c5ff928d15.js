0e0c45f20713b084c9c8a042d57443f87b9db3dd
unknown <gvsureshDev97>
2020-04-03 06:50:18 -0700

    Updates

---
+++ precache-manifest.2c7267b62109ff5954d497c5ff928d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c413904f978ad007523243ae52d0a62",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "68489c19858c569bb8c8",
+    "url": "/test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "efda1bf2b32ad9463961",
+    "url": "/test/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "68489c19858c569bb8c8",
+    "url": "/test/static/js/2.8ffa10cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.8ffa10cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efda1bf2b32ad9463961",
+    "url": "/test/static/js/main.7fe546ce.chunk.js"
+  },
+  {
+    "revision": "049932b6e5e5e49e9dac",
+    "url": "/test/static/js/runtime-main.027b8e1e.js"
+  }
+]);
